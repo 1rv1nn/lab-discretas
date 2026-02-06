8926cf24019a0ce3511c6e4735273c303eeff9d4
1rv1nn <1rv1n@ciencias.unam.mx>
2026-02-06 02:29:17 -0600

    Clean Branch

--- logicProp/Auxiliar.hs
+++
@@ -1,5 +0,0 @@
-module Auxiliar where
-
-contarLista :: [a] -> Int
-contarLista [] = 0
-contarLista (x:xs) = 1 + contarLista xs
--- modulosHaskell_2/Cuadrado.hs
+++
@@ -1,8 +0,0 @@
-module Cuadrado (cuadrado) where
-
-{- Función: cuadrado
-   Descripción: Devuelve el cuadrado de un número
-   Uso: cuadrado 4 = 16
--}
-cuadrado :: Int -> Int
-cuadrado x = x^2
